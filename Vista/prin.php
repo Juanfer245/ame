14d723aee190bf587076789f96f7038676628c04
PamelaMichelle <Pamelish030420>
2022-07-04 09:13:34 -0500

    primer borrador

---
+++ Vista/prin.php
@@ -0,0 +1,55 @@
+<?php 
+    include_once ('../Controlador/in.php'); 
+    include_once ('../Modelo/operacion.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CRUD</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css"
+        integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="styleprin.css">
+</head>
+
+<header class="h">
+    <h1>REGISTRO DE CURSOS DE UNA ACADEMIA DE BAILE</h1>
+</header>
+
+<body>
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th scope="col">Codigo del curso</th>
+                <th scope="col">Nombre del curso</th>
+                <th scope="col">Duracion del curso</th>
+                <th scope="col">Fecha de inicializacion</th>
+                <th scope="col">Fecha de finalizacion</th>
+                <th scope="col">Cedula del estudiante</th>
+            </tr>
+        </thead>
+
+        <tbody>
+            <?php foreach($registros as $record){ ?>
+            <tr>
+                <td class="is_integer"><?php echo "$record[codigo_curso]"?></td>
+                <td class="texto"><?php echo "$record[nombre_curso]"?></td>
+                <td class="texto"><?php echo "$record[duracion]"?></td>
+                <td class="texto"><?php echo "$record[fecha_inicio]"?></td>
+                <td class="is_integer"><?php echo "$record[fecha_final]"?></td>
+                <td class="is_integer"><?php echo "$record[cedula_estudiante]"?></td>
+                <td>
+                    <a href="update.php?update=<?=$record['codigo_curso']?>" class="btn btn-secondary btn-sm">editar</a>
+                    <a href="delete.php?delete=<?=$record['codigo_curso']?>" class="btn btn-danger  btn-sm">Eliminar</a>
+
+                </td>
+            </tr>
+            <?php } ?>
+
+        </tbody>
+    </table>
+</body>
+
+</html>
---
+++ Vista/update.php
@@ -0,0 +1,62 @@
+<?php 
+include_once '../Modelo/conexion.php';
+require_once '../Controlador/ac.php';
+
+
+?>
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registrar Categorias</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<header>
+    <h1 class="text-center">ACTUALIZAR INFORMACIÓN</h1>
+</header>
+
+<body>
+    <div class="container">
+
+        <form action="../Controlador/ac.php" class="form" id="form" autocomplete="off" method="POST">
+
+            <div class="form-group">
+                <label for="codig_curso">Codigo del curso</label>
+                <input type="is_integer" name="codigo_curso" required>
+            </div>
+
+            <div class="form-group">
+                <label for="nombre_curso">Nombre del curso</label>
+                <input type="text" name="nombre_curso" required>
+            </div>
+
+            <div class="form-group">
+                <label for="duracion">Duracion del curso</label>
+                <input type="text" name="duracion">
+            </div>
+
+            <div class="form-group">
+                <label for="fecha_inicial">Fecha de inicializacion</label>
+                <input type="text" name="fecha_inicio">
+            </div>
+
+            <div class="form-group">
+                <label for="fecha_final">Fecha de finalización</label>
+                <input type="text" name="fecha_final">
+            </div>
+
+            <div class="form-group">
+                <label for="cedula_estudiante">Cedula del estudiante</label>
+                <input type="is_integer" maxlength="10" name="cedula_estudiante">
+            </div>
+            <input type="submit" value="actualizar" href="../Vista/prin.php">
+            <a href="../Vista/prin.php">regresar</a>
+
+        </form>
+
+    </div>
+</body>
+
+</html>
